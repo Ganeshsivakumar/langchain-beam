be3a7b12cfb3446bb8fdd594997315bb336a9342
Ganesh <ganeshsivakumar85@gmail.com>
2025-08-15 17:18:01 +0530

    feat: kafka to pinecone template (#36)

---
+++ templates/kafka-to-pinecone/src/main/java/com/templates/langchainbeam/KafkaToPineconePipelineOptions.java
@@ -0,0 +1,45 @@
+package com.templates.langchainbeam;
+
+import org.apache.beam.sdk.options.PipelineOptions;
+
+public interface KafkaToPineconePipelineOptions extends PipelineOptions {
+
+    /** Kafka brokers, comma-separated. */
+    String getBrokers();
+
+    void setBrokers(String brokers);
+
+    /** Kafka topic to consume from. */
+    String getTopic();
+
+    void setTopic(String topic);
+
+    /** Embedding model name. */
+    String getEmbeddingModel();
+
+    void setEmbeddingModel(String model);
+
+    /** OpenAI API key. */
+    String getOpenaiApiKey();
+
+    void setOpenaiApiKey(String key);
+
+    /** Pinecone API key. */
+    String getPineconeApiKey();
+
+    void setPineconeApiKey(String key);
+
+    /** Pinecone index name. */
+    String getPineconeIndex();
+
+    void setPineconeIndex(String index);
+
+    /** Pinecone namespace. */
+    String getPineconeNamespace();
+
+    void setPineconeNamespace(String namespace);
+
+    String getPineconeApiUrl();
+
+    void setPineconeApiUrl(String url);
+}
